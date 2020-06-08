752a46c1d86e97da74671708d6f40533cc102c83
df2017 <dferrey22783@gmail.com>
2020-06-08 20:56:58 -0300

    MODIFY: file server

--- server.js
+++ server.js
@@ -13,15 +13,18 @@ const path = require('path');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/api/v1/auth", autenticacionController.login);
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
 
 app.get("/api/v1/tasks", middleware.autenticado, tareaController.obtenerTareas);
 app.post("/api/v1/tasks", middleware.autenticado, tareaController.guardarTarea);
 app.put("/api/v1/tasks/:id", middleware.autenticado, tareaController.actualizarTareas);
 app.delete("/api/v1/tasks/:id", middleware.autenticado, tareaController.eliminarTarea);
 
+app.post("/api/v1/auth", autenticacionController.login);
 // server config.
-const port = 5000;
+const port =  process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App todo iniciada en el puerto ${port}`);
